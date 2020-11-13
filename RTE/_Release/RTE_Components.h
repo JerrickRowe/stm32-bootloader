eb21870630e730410c36ec9451271640b462cfae
Jerrick <JerrickOn@Gmail.com>
2020-11-13 17:05:28 +0800

    Add usb device msc file structure

---
+++ RTE/_Release/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32-bootloader' 
+ * Target:  'Release' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/*  Keil.MDK-Plus::USB:Device:MSC:6.14.0 */
+#define RTE_USB_Device_MSC_0            /* USB Device MSC instance 0 */
+
+
+
+#endif /* RTE_COMPONENTS_H */
